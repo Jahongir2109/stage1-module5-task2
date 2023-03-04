7ac5ef63b5f1a315dbd1e2f6c91ae32b3a8f0ceb
Jahongir2109 <jahongirachilov004@gmail.com>
2023-03-04 14:32:44 +0500

    lamda fixed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Operation<Integer>operation=new Operation<Integer>() {
+            @Override
+            public List<Integer> apply(List<Integer> arg) {
+                List<Integer>integers=new ArrayList<>();
+                for (Integer integer : arg) {
+                    integers.add(integer/3);
+                }
+                return integers;
+            }
+        };
+        return operation;
     }
 }
